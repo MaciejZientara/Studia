3fc173384553158fe8650b88dade6783399d93be
MaciejZientara <mtzientara@gmail.com>
2021-12-07 09:36:53 +0100

    dzialajacy python!

--- python/lista8/zad1.py
+++ python/lista8/zad1.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
-from sqlalchemy import Table, Column, Integer, ForeignKey, String, DateTime, create_engine, select
+from sqlalchemy import Table, Column, Integer, ForeignKey, String, DateTime, create_engine, select, Boolean
 from sqlalchemy.orm import sessionmaker, validates
 import sys
 
@@ -32,6 +32,7 @@ class Ksiazka(Base):
     nazwa = Column(String(30), nullable=False)
     autor = Column(String(50), nullable=False)
     rok = Column(Integer, nullable=False)
+    wypozyczono = Column(Boolean, nullable=False, default=False)
 
 class Wypozyczenie(Base):
     __tablename__ = 'Wypozyczenie'
@@ -52,12 +53,12 @@ def dodaj_osobe():
         print('prosze podac', end = ' ')
     if len(sys.argv) < 4:
         print('Osoba.imie', end = ' ')
-        return
     if len(sys.argv) < 5:
         print('Osoba.nazwisko', end = ' ')
-        return
     if len(sys.argv) < 6:
         print('Osoba.email', end = ' ')
+    if len(sys.argv) < 6:
+        print("\n")
         return
     sesja = Session()
     o = Osoba(imie = sys.argv[3], nazwisko = sys.argv[4], email = sys.argv[5])
@@ -70,12 +71,12 @@ def dodaj_ksiazke():
         print('prosze podac', end = ' ')
     if len(sys.argv) < 4:
         print('Ksiazka.nazwa', end = ' ')
-        return
     if len(sys.argv) < 5:
         print('Ksiazka.autor', end = ' ')
-        return
     if len(sys.argv) < 6:
         print('Ksiazka.rok', end = ' ')
+    if len(sys.argv) < 6:
+        print("\n")
         return
     sesja = Session()
     k = Ksiazka(nazwa = sys.argv[3], autor = sys.argv[4], rok = int(sys.argv[5]))
@@ -88,12 +89,12 @@ def usun_osobe():
         print('prosze podac', end = ' ')
     if len(sys.argv) < 4:
         print('Osoba.imie', end = ' ')
-        return
     if len(sys.argv) < 5:
         print('Osoba.nazwisko', end = ' ')
-        return
     if len(sys.argv) < 6:
         print('Osoba.email', end = ' ')
+    if len(sys.argv) < 6:
+        print("\n")
         return
     sesja = Session()
     o = Osoba(imie = sys.argv[3], nazwisko = sys.argv[4], email = sys.argv[5])
@@ -106,12 +107,12 @@ def usun_ksiazke():
         print('prosze podac', end = ' ')
     if len(sys.argv) < 4:
         print('Ksiazka.nazwa', end = ' ')
-        return
     if len(sys.argv) < 5:
         print('Ksiazka.autor', end = ' ')
-        return
     if len(sys.argv) < 6:
         print('Ksiazka.rok', end = ' ')
+    if len(sys.argv) < 6:
+        print("\n")
         return
     sesja = Session()
     k = Ksiazka(nazwa = sys.argv[3], autor = sys.argv[4], rok = int(sys.argv[5]))
@@ -124,26 +125,24 @@ def usun_wypozyczenie():
         print('prosze podac', end = ' ')
     if len(sys.argv) < 4:
         print('Osoba.imie', end = ' ')
-        return
     if len(sys.argv) < 5:
         print('Osoba.nazwisko', end = ' ')
-        return
     if len(sys.argv) < 6:
         print('Osoba.email', end = ' ')
-        return
     if len(sys.argv) < 7:
         print('Ksiazka.nazwa', end = ' ')
-        return
     if len(sys.argv) < 8:
         print('Ksiazka.autor', end = ' ')
-        return
     if len(sys.argv) < 9:
         print('Ksiazka.rok', end = ' ')
+    if len(sys.argv) < 9:
+        print("\n")
         return
     sesja = Session()
-    id_ksiazki = (sesja.query(Ksiazka).filter(Ksiazka.nazwa == sys.argv[6] and Ksiazka.autor == sys.argv[7] and Ksiazka.rok == int(sys.argv[8])).all())[0].id
-    id_osoby = (sesja.query(Osoba).filter(Osoba.imie == sys.argv[3] and Osoba.nazwisko == sys.argv[4] and Osoba.email == sys.argv[5]).all())[0].id
-    w = (sesja.query(Wypozyczenie).filter(Wypozyczenie.id_ksiazka == id_ksiazki and Wypozyczenie.id_osoba == id_ksiazki))[0]    
+    id_ksiazki = (sesja.query(Ksiazka).filter(Ksiazka.nazwa == sys.argv[6] and Ksiazka.autor == sys.argv[7] and Ksiazka.rok == int(sys.argv[8])).all())[0]
+    id_osoby = (sesja.query(Osoba).filter(Osoba.imie == sys.argv[3] and Osoba.nazwisko == sys.argv[4] and Osoba.email == sys.argv[5]).all())[0]
+    w = (sesja.query(Wypozyczenie).filter(Wypozyczenie.id_ksiazka == id_ksiazki.id and Wypozyczenie.id_osoba == id_osoby.id))[0]
+    id_ksiazki.wypozyczono = False
     sesja.delete(w)
     sesja.commit()
     sesja.close()
@@ -153,21 +152,18 @@ def wypozyczenie_ksiazki():
         print('prosze podac', end = ' ')
     if len(sys.argv) < 4:
         print('Osoba.imie', end = ' ')
-        return
     if len(sys.argv) < 5:
         print('Osoba.nazwisko', end = ' ')
-        return
     if len(sys.argv) < 6:
         print('Osoba.email', end = ' ')
-        return
     if len(sys.argv) < 7:
         print('Ksiazka.nazwa', end = ' ')
-        return
     if len(sys.argv) < 8:
         print('Ksiazka.autor', end = ' ')
-        return
     if len(sys.argv) < 9:
         print('Ksiazka.rok', end = ' ')
+    if len(sys.argv) < 9:
+        print("\n")
         return
     sesja = Session()
     id_ksiazki = sesja.query(Ksiazka).filter(Ksiazka.nazwa.like(sys.argv[6]), Ksiazka.autor.like(sys.argv[7]), Ksiazka.rok.like(sys.argv[8])).all()
@@ -175,8 +171,12 @@ def wypozyczenie_ksiazki():
     # id_ksiazki = sesja.query(Ksiazka).filter(Ksiazka.nazwa == sys.argv[6] and Ksiazka.autor == sys.argv[7] and Ksiazka.rok == int(sys.argv[8])).all()
     # id_osoby = sesja.query(Osoba).filter(Osoba.imie == sys.argv[3] and Osoba.nazwisko == sys.argv[4] and Osoba.email == sys.argv[5]).all()
     w = Wypozyczenie(id_ksiazka = id_ksiazki[0].id, id_osoba = id_osoby[0].id)
-    sesja.add(w)
-    sesja.commit()
+    if id_ksiazki[0].wypozyczono == False:
+        id_ksiazki[0].wypozyczono = True
+        sesja.add(w)
+        sesja.commit()
+    else:
+        print("Ksiazka juz wypozyczona!")
     sesja.close()
 
 def zwrot_ksiazki():
@@ -184,27 +184,25 @@ def zwrot_ksiazki():
         print('prosze podac', end = ' ')
     if len(sys.argv) < 4:
         print('Osoba.imie', end = ' ')
-        return
     if len(sys.argv) < 5:
         print('Osoba.nazwisko', end = ' ')
-        return
     if len(sys.argv) < 6:
         print('Osoba.email', end = ' ')
-        return
     if len(sys.argv) < 7:
         print('Ksiazka.nazwa', end = ' ')
-        return
     if len(sys.argv) < 8:
         print('Ksiazka.autor', end = ' ')
-        return
     if len(sys.argv) < 9:
         print('Ksiazka.rok', end = ' ')
+    if len(sys.argv) < 9:
+        print("\n")
         return
     sesja = Session()
-    id_ksiazki = (sesja.query(Ksiazka).filter(Ksiazka.nazwa == sys.argv[6] and Ksiazka.autor == sys.argv[7] and Ksiazka.rok == int(sys.argv[8])).all())[0].id
-    id_osoby = (sesja.query(Osoba).filter(Osoba.imie == sys.argv[3] and Osoba.nazwisko == sys.argv[4] and Osoba.email == sys.argv[5]).all())[0].id
-    w = (sesja.query(Wypozyczenie).filter(Wypozyczenie.id_ksiazka == id_ksiazki and Wypozyczenie.id_osoba == id_ksiazki))[-1]#dodaj do ostatniego pasujacego wypozyczenia
+    id_ksiazki = (sesja.query(Ksiazka).filter(Ksiazka.nazwa == sys.argv[6] and Ksiazka.autor == sys.argv[7] and Ksiazka.rok == int(sys.argv[8])).all())[0]
+    id_osoby = (sesja.query(Osoba).filter(Osoba.imie == sys.argv[3] and Osoba.nazwisko == sys.argv[4] and Osoba.email == sys.argv[5]).all())[0]
+    w = (sesja.query(Wypozyczenie).filter(Wypozyczenie.id_ksiazka == id_ksiazki.id and Wypozyczenie.id_osoba == id_osoby.id))[-1]#dodaj do ostatniego pasujacego wypozyczenia
     w.data_oddania = func.now()
+    id_ksiazki.wypozyczono = False
     sesja.commit()
     sesja.close()
 
@@ -237,22 +235,26 @@ if len(sys.argv) == 1:
 else:
     if sys.argv[1] == 'wypisz':
         printAll()
-    if sys.argv[1] == 'dodaj':
-        if sys.argv[2] == 'ksiazka':
-            dodaj_ksiazke()
-        if sys.argv[2] == 'osoba':
-            dodaj_osobe()
-        if sys.argv[2] == 'wypozyczenie':
-            wypozyczenie_ksiazki()
-        if sys.argv[2] == 'zwrot':
-            zwrot_ksiazki()
-    if sys.argv[1] == 'usun':
-        if sys.argv[2] == 'ksiazka':
-            usun_ksiazke()
-        if sys.argv[2] == 'osoba':
-            usun_osobe()
-        if sys.argv[2] == 'wypozyczenie':
-            usun_wypozyczenie()
+    else:
+        if len(sys.argv) < 3:
+            print("za malo argumentow")
+        else:
+            if sys.argv[1] == 'dodaj':
+                if sys.argv[2] == 'ksiazka':
+                    dodaj_ksiazke()
+                if sys.argv[2] == 'osoba':
+                    dodaj_osobe()
+                if sys.argv[2] == 'wypozyczenie':
+                    wypozyczenie_ksiazki()
+                if sys.argv[2] == 'zwrot':
+                    zwrot_ksiazki()
+            if sys.argv[1] == 'usun':
+                if sys.argv[2] == 'ksiazka':
+                    usun_ksiazke()
+                if sys.argv[2] == 'osoba':
+                    usun_osobe()
+                if sys.argv[2] == 'wypozyczenie':
+                    usun_wypozyczenie()
 
 
 # python3 zad1.py dodaj ksiazka hp jkr 2000
@@ -260,4 +262,11 @@ else:
 # python3 zad1.py dodaj wypozyczenie Jan Kowalski JK@gmail.com hp jkr 2000
 # python3 zad1.py wypisz
 # python3 zad1.py dodaj zwrot Jan Kowalski JK@gmail.com hp jkr 2000
+# python3 zad1.py wypisz
+# python3 zad1.py dodaj wypozyczenie Jan Kowalski JK@gmail.com hp jkr 2000
+# python3 zad1.py wypisz
+# python3 zad1.py dodaj osoba Anna Dudek AD@gmail.com
+# python3 zad1.py dodaj wypozyczenie Anna Dudek AD@gmail.com hp jkr 2000
+# python3 zad1.py dodaj zwrot Jan Kowalski JK@gmail.com hp jkr 2000
+# python3 zad1.py dodaj wypozyczenie Anna Dudek AD@gmail.com hp jkr 2000
 # python3 zad1.py wypisz
