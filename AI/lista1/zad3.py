5e84337d263f26de18e7dca3243a9d907a02f4b0
MaciejZientara <mtzientara@gmail.com>
2022-03-04 01:04:56 +0100

    zad3 toFinish

--- AI/lista1/zad3.py
+++ AI/lista1/zad3.py
@@ -18,6 +18,7 @@ import random
 # black spades (♠), red hearts (♥), blue diamonds (♦) and green clubs (♣)
 colors = ['♠','♥','♦','♣']
 figures = ['A','K','Q','J']
+number = ['2','3','4','5','6','7','8','9','10']
 
 def drawHandFigures():
     res = []
@@ -32,23 +33,51 @@ def whatSet(hand):
     for card in hand:
         figuresCount[figures.index(card[0])]+=1
     if 4 in figuresCount:
-        print('four of a kind')
+        # print('four of a kind')
         return 3
     if 3 in figuresCount and 2 in figuresCount:
-        print('full house')
+        # print('full house')
         return 4
     if 3 in figuresCount:
-        print('three of a kind')
+        # print('three of a kind')
         return 7
     pairCount = sum(x==2 for x in figuresCount)
     if pairCount == 2:
-        print('two pair')
+        # print('two pair')
         return 8
     if pairCount == 1:
-        print('one pair')
+        # print('one pair')
         return 9
     
+
+customDeck = [  '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠',
+                '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥',
+                '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦',
+                '2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣']
+
+def calculateChance(hand):
+    colorsCount = [0,0,0,0]
+    numberCount = [0,0,0,0,0,0,0,0,0]
+    
+    for card in customDeck:
+        for c in colors:
+            if c in card:
+                colorsCount[colors.index(c)]+=1
+        for n in number:
+            if n in card:
+                numberCount[number.index(n)]+=1
+    print('colorsCount =', colorsCount, 'numberCount =', numberCount)
+    chances = [0,0,0,0,0,0,0,0,0,0]
+    chances[0] = 0 #royal flush
+
+    #                                                                               TODO  !!!
+
+    chances[10] = 0 #high card
+
+    return sum(chances[i] for i in range(whatSet(hand))) # suma szans lepszych ustawien niz przeciwnika
+
 
 hand = drawHandFigures()
 print(hand)
 print(whatSet(hand))
+print(calculateChance(hand))
