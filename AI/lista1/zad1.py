a73ce874c61abcad8e513a310fffb86cf9551e4c
MaciejZientara <mtzientara@gmail.com>
2022-03-06 01:39:57 +0100

    poprawienie czytelnosci, DFS -> BFS

--- AI/lista1/zad1.py
+++ AI/lista1/zad1.py
@@ -1,7 +1,5 @@
 from copy import deepcopy
 
-moves = 0
-
 # plansza
 # 8
 # |
@@ -21,119 +19,100 @@ def moveL(figure):
     return chr(ord(figure[0])-1) + figure[1]
 
 def moveUL(figure):
-    return moveU(moveL(figure))
+    return chr(ord(figure[0])-1) + chr(ord(figure[1])+1)
     
 def moveUR(figure):
-    return moveU(moveR(figure))
+    return chr(ord(figure[0])+1) + chr(ord(figure[1])+1)
 
 def moveDL(figure):
-    return moveD(moveL(figure))    
+    return chr(ord(figure[0])-1) + chr(ord(figure[1])-1)
     
 def moveDR(figure):
-    return moveD(moveR(figure))
-    
-def isValidMove(wKing,wRook,bKing):
-    if (wKing[0] < 'a' or wKing[0] > 'h' or wKing[1] < '1' or wKing[1] > '8' or 
-        wRook[0] < 'a' or wRook[0] > 'h' or wRook[1] < '1' or wRook[1] > '8' or 
-        bKing[0] < 'a' or bKing[0] > 'h' or bKing[1] < '1' or bKing[1] > '8'):
-        return False #wyjscie poza plansze
-    if wKing == bKing or wRook == bKing or wRook == wKing:
-        return False #wejscie na inna figure
-    if (abs(ord(wKing[0]) - ord(bKing[0])) <= 1) and (abs(ord(wKing[1]) - ord(bKing[1])) <= 1):
-        return False #wejscie krola w zakres krola
-    if (bKing[0] == wRook[0]) or (bKing[1] == wRook[1]):
-        return False #wejscie krola pod wieze
-    return True
+    return chr(ord(figure[0])+1) + chr(ord(figure[1])-1)
+
+kingMoves = [moveL,moveD,moveR,moveU,moveUL,moveDL,moveDR,moveUR]
+rookMoves = [moveL,moveD,moveR,moveU]
 
 def isCheck(wKing,wRook,bKing):#czarny krol szachowany przez wieze
     return (bKing[0]==wRook[0]) or (bKing[1]==wRook[1])
 
+def isOutsideBoard(figure):
+    return figure[0] < 'a' or figure[0] > 'h' or figure[1] < '1' or figure[1] > '8'
+
+def areKingsTooClose(king1,king2):
+    return (abs(ord(king1[0]) - ord(king2[0])) <= 1) and (abs(ord(king1[1]) - ord(king2[1])) <= 1)
+
 def isPat(wKing,wRook,bKing):#czarny krol nie moze wykonac zadnego ruchu
-    return not (isValidMove(wKing,wRook,moveL(bKing)) or
-                isValidMove(wKing,wRook,moveR(bKing)) or
-                isValidMove(wKing,wRook,moveD(bKing)) or
-                isValidMove(wKing,wRook,moveU(bKing)) or
-                isValidMove(wKing,wRook,moveUL(bKing)) or
-                isValidMove(wKing,wRook,moveUR(bKing)) or
-                isValidMove(wKing,wRook,moveDR(bKing)) or
-                isValidMove(wKing,wRook,moveDL(bKing)))
+    for move in kingMoves:
+        tmpKing = move(bKing)
+        if not (isOutsideBoard(tmpKing) or isCheck(wKing,wRook,tmpKing) or areKingsTooClose(wKing,tmpKing)):
+            return False
+    return True
+    #pat kiedy wszystkie ruchy sa niepoprawne, jesli ruch poprawny to return false
+    
+def isValidMove(turn,wKing,wRook,bKing):
+    if wKing == bKing or wRook == bKing or wRook == wKing:
+        return False #wejscie na inna figure
+    if areKingsTooClose(wKing,bKing):
+        return False #wejscie krola w zakres krola
+    if turn == 'white' and isCheck(wKing,wRook,bKing):#czarny zrobil ruch, tura bialego
+        return False #czarny dobrowolnie wszedl pod wieze
+    if turn == 'black' and isPat(wKing,wRook,bKing) and not isCheck(wKing,wRook,bKing):
+        return False #pat = fail, pat+check = win <- nie mozna zakazac takiego ruchu!
+    return True
 
 def isWin(wKing,wRook,bKing):#krol szachowany i nie moze zrobic ruchu
     return isCheck(wKing,wRook,bKing) and isPat(wKing,wRook,bKing)
 
-def setMoves(count):
-    global moves
-    moves = count
+def queueInsert(que,turn,wKing,wRook,bKing,count,path):
+    if count > 30:
+        return #nie udalo sie znalezc rozwiazania, czyszczenie listy
 
-def round(turn,wKing,wRook,bKing,count,path):
-    if not __debug__:
-        path = deepcopy(path) #aby w kazdym wywolaniu funkcji nie bylo referencji do tego samego obiektu, tylko nowa lista
-        path.append([wKing,wRook,bKing])
-    if count > moves:
-        return False #fail
-    if isWin(wKing,wRook,bKing):
-        setMoves(count)
+    if isValidMove(turn,wKing,wRook,bKing):
+        que.append([turn,wKing,wRook,bKing,count,path])
+        if isWin(wKing,wRook,bKing):
+            que[0],que[-1] = que[-1],que[0] #znalezione zwyciestwo, w nastepnym while w round wyjscie z petli
+            # print('win',que[0], isWin(que[0][1],que[0][2],que[0][3]))
+    return
+
+def round(Turn,whiteKing,whiteRook,blackKing):
+    que = []
+
+    que.append([Turn,whiteKing,whiteRook,blackKing,0,[]])
+
+    while que: # que not empty
+        [turn,wKing,wRook,bKing,count,path] = que.pop(0)
+
+        # print(turn,wKing,wRook,bKing,count)
+
         if not __debug__:
-            # print(path)
-            out.write(str(path)+'\n')
-        return True #poniewaz w ValidMove sprawdzam czy jest pod wieza - wyklucza szach
-    if not isValidMove(wKing,wRook,bKing):
-        return False #niepoprawny ruch
-    if turn == 'white' and isCheck(wKing,wRook,bKing):
-        return False #czarny dobrowolnie wszedl pod wieze
+            path = deepcopy(path) #aby w kazdym kroku nie bylo referencji do tego samego obiektu, tylko nowa lista
+            path.append([wKing,wRook,bKing])
 
-    if turn == 'black' and isPat(wKing,wRook,bKing):
-        return False
+        if isWin(wKing,wRook,bKing):
+            if not __debug__:
+                # print(path)
+                out.write(str(path)+'\n')
+            return count
 
-    if turn == 'black':
-        return (round('white',wKing,wRook,moveL(bKing) ,count+1,path) or
-                round('white',wKing,wRook,moveR(bKing) ,count+1,path) or
-                round('white',wKing,wRook,moveD(bKing) ,count+1,path) or
-                round('white',wKing,wRook,moveU(bKing) ,count+1,path) or
-                round('white',wKing,wRook,moveDR(bKing),count+1,path) or
-                round('white',wKing,wRook,moveDL(bKing),count+1,path) or
-                round('white',wKing,wRook,moveUR(bKing),count+1,path) or
-                round('white',wKing,wRook,moveUL(bKing),count+1,path))
-    else: #white
-        if (round('black',moveL(wKing) ,wRook,bKing,count+1,path) or
-            round('black',moveR(wKing) ,wRook,bKing,count+1,path) or
-            round('black',moveD(wKing) ,wRook,bKing,count+1,path) or
-            round('black',moveU(wKing) ,wRook,bKing,count+1,path) or
-            round('black',moveDR(wKing),wRook,bKing,count+1,path) or
-            round('black',moveDL(wKing),wRook,bKing,count+1,path) or
-            round('black',moveUR(wKing),wRook,bKing,count+1,path) or
-            round('black',moveUL(wKing),wRook,bKing,count+1,path)):
-            return True
-
-        tmp = wRook
-        for i in range(8):
-            tmp = moveL(tmp)
-            if tmp == bKing or tmp == wKing:
-                break
-            if round('black',wKing,tmp,bKing,count+1,path):
-                return True
-        tmp = wRook
-        for i in range(8):
-            tmp = moveD(tmp)
-            if tmp == bKing or tmp == wKing:
-                break
-            if round('black',wKing,tmp,bKing,count+1,path):
-                return True
-        tmp = wRook
-        for i in range(8):
-            tmp = moveR(tmp)
-            if tmp == bKing or tmp == wKing:
-                break
-            if round('black',wKing,tmp,bKing,count+1,path):
-                return True
-        tmp = wRook
-        for i in range(8):
-            tmp = moveU(tmp)
-            if tmp == bKing or tmp == wKing:
-                break
-            if round('black',wKing,tmp,bKing,count+1,path):
-                return True
-    return False
+        if turn == 'black':
+            for move in kingMoves:
+                BKing = move(bKing)
+                if not isOutsideBoard(BKing):
+                    queueInsert(que,'white',wKing,wRook,BKing,count+1,path)
+        else: #white
+            for move in kingMoves:
+                WKing = move(wKing)
+                if not isOutsideBoard(WKing):
+                    queueInsert(que,'black',WKing,wRook,bKing,count+1,path)
+            for mv in rookMoves:
+                tmp = wRook
+                for i in range(8):
+                    tmp = mv(tmp)
+                    if tmp == bKing or tmp == wKing or isOutsideBoard(tmp): #wieza natrafila na przeszkode, nie moze dalej isc
+                        break
+                    queueInsert(que,'black',wKing,tmp,bKing,count+1,path)
+    return 'INF'
 
 
 with open('zad1_input.txt','r') as file:
@@ -141,16 +120,9 @@ with open('zad1_input.txt','r') as file:
         for line in file:
             [turn,wKing,wRook,bKing] = line.split()
             found = False
-            for i in range(50):
-                moves = i+1
-                if round(turn,wKing,wRook,bKing,0,[]):
-                    found = True
-                    # print('udalo sie w',moves,'ruchach!')
-                    out.write(str(moves)+'\n')
-                    break
-            if not found:
-                # print('INF')
-                out.write('INF\n')
+            res = round(turn,wKing,wRook,bKing)
+            # print(res)
+            out.write(str(res)+'\n')
 
 # black f2 h6 h1
 # white f2 c6 h1
