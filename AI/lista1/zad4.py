23c0c041366222f9ddc18a7adba7192a8b01788b
MaciejZientara <mtzientara@gmail.com>
2022-03-15 14:39:17 +0100

    liniowe rozwiazanie zad4

--- AI/lista1/zad4.py
+++ AI/lista1/zad4.py
@@ -5,19 +5,18 @@
 # wynik to ilosc zmian + ilosc pozostalych 1
 
 def nonogram(row, n):
-    oneCount = sum([r=='1' for r in row])
-    res = len(row)
-    for i in range(len(row)-n+1):
-        changes = 0
-        oneCopy = oneCount
-        for j in range(n):
-            if row[i+j] == '0':
-                changes+=1
-            else:
-                oneCopy-=1
-        changes+=oneCopy
-        res = min(res,changes)
+    switch = sum([r=='1' for r in row])
+    res = switch + sum([1 if row[i]=='0' else -1 for i in range(n)])
+    tmp = res
+    for i in range(len(row)-n):
+        tmp+= (1 if row[i]=='1' else -1)
+        tmp+= (-1 if row[i+n]=='1' else 1)
+        res = min(res,tmp)
     return res
+
+# najpierw sprawdzam pierwsze n pozycji ile tam jest zmian
+# potem gąsiennicowo przesuwam się o jeden, usuwam zmiane wykonaną na poprzedniej pozycji i dodaje ilość zmian na następnej pozycji
+# czyli najpierw [0,n], [1,n+1], [2,n+2], ... i sprawdzam liniowo całość, a potem biorę minimum z tych ramek
 
 with open('zad4_output.txt','w') as out:
     with open('zad4_input.txt','r') as file:
