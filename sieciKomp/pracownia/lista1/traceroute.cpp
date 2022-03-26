0f6fb968ed4179bcb357dc58e74cfdc3413a030b
MaciejZientara <mtzientara@gmail.com>
2022-03-26 02:57:46 +0100

    sieci

---
+++ sieciKomp/pracownia/lista1/traceroute.cpp
@@ -0,0 +1,187 @@
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <cassert>
+#include <unistd.h>
+#include <sys/select.h>
+#include <netinet/ip_icmp.h>
+
+#include <chrono>
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::chrono::system_clock;
+
+
+pid_t PID;
+int sockfd;
+
+u_int16_t compute_icmp_checksum (const void *buff, int length)
+{
+	u_int32_t sum;
+	const u_int16_t* ptr = (u_int16_t*)buff;
+	assert (length % 2 == 0);
+	for (sum = 0; length > 0; length -= 2)
+		sum += *ptr++;
+	sum = (sum >> 16) + (sum & 0xffff);
+	return (u_int16_t)(~(sum + (sum >> 16)));
+}
+
+unsigned short swapBytes(unsigned short x){
+    return ((x&0xff00)>>8) | ((x&0xff)<<8);
+}
+
+void print_as_bytes (unsigned char* buff, ssize_t length)
+{
+	for (ssize_t i = 0; i < length; i++, buff++)
+		printf ("%.2x ", *buff);	
+}
+
+
+int main(int argc, char** argv){
+    //sprawdzenie ilosci argumentow
+    if(argc != 2){
+		fprintf(stderr, "Only argument should be IP address\n"); 
+		return EXIT_FAILURE;
+    }
+
+    //ustawienie adresu odbiorcy
+    sockaddr_in recipient;
+    bzero (&recipient, sizeof(recipient));
+    recipient.sin_family = AF_INET;
+    inet_pton(AF_INET, argv[1], &recipient.sin_addr);
+
+    //ustawienie file descriptor
+	sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (sockfd < 0) {
+		fprintf(stderr, "socket error: %s\n", strerror(errno)); 
+		return EXIT_FAILURE;
+	}
+
+    // ustawienie odpowiedniego naglowka pakietu
+    PID = getpid();
+    struct icmp header;
+    header.icmp_type = ICMP_ECHO;
+    header.icmp_code = 0;
+    header.icmp_hun.ih_idseq.icd_id = PID;//unikatowy identyfikator - id procesu
+    header.icmp_hun.ih_idseq.icd_seq = 0;//numer sekwencyjny
+    header.icmp_cksum = 0;
+    header.icmp_cksum = compute_icmp_checksum((u_int16_t*)&header, sizeof(header));
+
+    //przygotowanie select - usypianie czekajac na file descriptor
+    fd_set rfds;
+    timeval tv;
+    int retval;
+
+    FD_ZERO(&rfds);
+    FD_SET(sockfd, &rfds);//select bedzie nasluchiwal na file descriptor socket
+
+    tv.tv_sec = 2;//czekamy co najwyzej sekunde
+    tv.tv_usec = 0;
+
+    
+	for (uint ttlCounter = 1;ttlCounter<30;ttlCounter++) {
+        int64_t startTime = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+
+        printf("%d. ", ttlCounter);
+        setsockopt (sockfd, IPPROTO_IP, IP_TTL, &ttlCounter, sizeof(int));//ustawiamy nowy ttl
+		
+        size_t bytes_sent;
+        int repeat = 3;
+        size_t headerLen = sizeof(header);
+        while(repeat > 0){
+            bytes_sent = sendto(sockfd, &header, headerLen, 0, (struct sockaddr*)&recipient, sizeof(recipient));
+            if(bytes_sent == headerLen)
+                repeat--;
+        }//wysylam 3 ping, sprawdzam tez czy wyslaly sie pelne header - jesli nie to powtarzam ping
+
+		sockaddr_in sender;	
+		socklen_t sender_len = sizeof(sender);
+		u_int8_t buffer[IP_MAXPACKET];
+
+
+		char sender_ip[3][25];
+        int64_t endTime[3];
+        
+        sender_ip[0][0] = '*';
+        sender_ip[1][0] = '*';//brak danych, potrzebne gdy nie odebralismy pakietow
+        sender_ip[2][0] = '*';
+
+        for(repeat=3;repeat>0;repeat--){
+            retval = select(sockfd+1, &rfds, NULL, NULL, &tv);//usypanie, az bedzie mozna odczytac
+
+            if(retval == -1){
+                //wystapil blad przy select
+                repeat++;//powtorz select
+                continue;
+            }
+            if(retval == 0){
+                //timeout
+                break;
+            }
+            //mozna odebrac pakiet
+
+    		ssize_t packet_len = recvfrom (sockfd, buffer, IP_MAXPACKET, 0, (struct sockaddr*)&sender, &sender_len);
+            if (packet_len < 0) {
+                fprintf(stderr, "recvfrom error: %s\n", strerror(errno)); 
+                return EXIT_FAILURE;
+            }
+
+            //odczytanie IP header i icmp header
+            ip* ip_header = (ip*) buffer;
+            ssize_t ip_header_len = 4 * ip_header->ip_hl;
+            u_int8_t* icmp_packet = buffer + ip_header_len;
+            icmp* icmp_header = (icmp*) icmp_packet;
+            
+            //sprawdzam czy pakiet nalezy do mnie (czy id == PID)
+
+            if(icmp_header->icmp_type == 11){//time exceeded
+                ip* ip_head = (ip*) (icmp_packet+8);//wyliczone ze struktury icmp time exceeded
+                ssize_t ip_head_len = 4 * ip_head->ip_hl;
+                u_int8_t* old_packet = icmp_packet+8 + ip_head_len;
+                icmp* old_icmp = (icmp*) old_packet;
+                if(old_icmp->icmp_hun.ih_idseq.icd_id != PID){
+                    repeat++;//powtorz select, otrzymalismy nie nalezacy do nas pakiet
+                    continue;
+                }                
+            }
+            if(icmp_header->icmp_type == 0){//ping reply
+                if(icmp_header->icmp_hun.ih_idseq.icd_id != PID){
+                    repeat++;//powtorz select, otrzymalismy nie nalezacy do nas pakiet
+                    continue;
+                }
+                ttlCounter = 100;//otrzymalismy odpowiedz od serwera, pozwala zakonczyc petle for
+            }
+
+            //zapisz ile czasu potrzeba bylo na odebranie tego pakietu (w milisekundach)
+            endTime[3-repeat] = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() - startTime;
+            //zapisz adres ip routera
+    		inet_ntop(AF_INET, &(sender.sin_addr), sender_ip[3-repeat], sizeof(sender_ip[3-repeat]));
+        }
+        if(sender_ip[0][0]=='*' && sender_ip[1][0]=='*' && sender_ip[2][0]=='*')
+            printf("*");//nie odebrano zadnego pakietu
+        else{
+            if(sender_ip[0][0]!='*')
+                printf("%s ", sender_ip[0]);
+    
+            if(sender_ip[1][0]!='*' && strcmp(sender_ip[0],sender_ip[1])!=0)
+                printf("%s ", sender_ip[1]);
+    
+            if(sender_ip[2][0]!='*' && strcmp(sender_ip[0],sender_ip[2])!=0 && strcmp(sender_ip[1],sender_ip[2])!=0 )
+                printf("%s ", sender_ip[2]);
+
+
+            if(sender_ip[0][0]=='*' || sender_ip[1][0]=='*' || sender_ip[2][0]=='*')
+                printf("???");//nie odebrano wszystkich 3 pakietow
+            else
+                printf("%ld ms", (endTime[0]+endTime[1]+endTime[2])/3);
+        }
+
+        printf("\n");
+	}
+
+	return EXIT_SUCCESS;
+}
