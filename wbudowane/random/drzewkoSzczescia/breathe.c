5e5cc95f069701f0a901c9c0d07e1a4505cd63b4
MaciejZientara <mtzientara@gmail.com>
2022-04-27 23:05:44 +0200

    wersja 1

--- wbudowane/random/drzewkoSzczescia/breathe.c
+++ wbudowane/random/drzewkoSzczescia/breathe.c
@@ -10,83 +10,71 @@ else zmieniam ustawienie portu (ktore diody zapalam)
 */
 
 //counter to ile cykli zegara przy okreslonym wypelnieniu
-//state to maska do PORT uzywana zapalania okreslonych diod
-volatile uint8_t counter, state;
+//ledMask to maska do PORT uzywana zapalania okreslonych diod
+//state odpowiada za wypelnienie (OCR0A = 1<<state)
+//w zaleznosci od ud (1=up,0=down) wykonuje state++/state--
+volatile uint8_t counter, state, ledMask, ud;
+// przypisuje counter=COUNTER
+#define COUNTER 10
 
 #define PORT PORTB
 #define DDR DDRB
 
-#define N 6
-#define WAIT 2
-
 void timer_init(){
   // ustaw tryb licznika
-  // COM1A = 10   -- fast PWM non-inverting mode
-  // WGM1  = 1110 -- fast PWM top=ICR1
-  // CS1   = 011  -- prescaler 64
+  // fast PWM mode
+  // WGM0  = 011 -- fast PWM top=0xFF
+  // CS0   = 100  -- prescaler 256
 
-  // częstotliwość 16e6/(1024*(1+15624)) = 1 Hz
-  // wzór: datasheet 20.12.3 str. 164
+  // częstotliwość 4e6/(256*(1+255)) = 61 Hz
 
-  ICR1 = WAIT<<N;
-  OCR1A = WAIT;
-  TCCR1A = _BV(COM1A1) | _BV(COM1A0) | _BV(WGM11);
-  TCCR1B = _BV(WGM12) | _BV(WGM13) | _BV(CS10) | _BV(CS11);
-  // ustaw pin OC1A (PB1) jako wyjście
-  DDRB |= _BV(PB1);
+  OCR0A = 1;
+  TCCR0A = _BV(WGM01) | _BV(WGM00);
+  TCCR0B = /*_BV(WGM02) |*/ _BV(CS02);
+
+  // wlacz interrupt na overflow i compare match
+  TIMSK0 = _BV(OCIE0A) | _BV(TOIE0);
 }
 
-void breathe(){ //zwiekszamy czas trwania zapalonej diody zmieniając wartość OCR1A
-  int8_t color = (rand()%7)+1; //                            TODO hue randomize
-  color ^= 0x7;
-  PORTC = color; // ustaw piny na kolor
-
-  OCR1A = 0;
-  PORTB |= _BV(PB1);
-  colorControl(color,50);
-  OCR1A = WAIT;
-
-  for(int8_t i=N-1; i>=0; i--){
-    TCNT1 = 0;
-    helper(color,i);
-  }
-  for(int8_t i=0; i<=N; i++){
-    TCNT1 = 0;
-    helper(color,i*2);
-  }
-
-  OCR1A = 0;
-  PORTB |= _BV(PB1);
-  colorControl(color,50);
-}
-
-//TODO dodac obsluge zmiany wypelnienia PWM
-// to ta funkcja powinna korzystac z counter,
-// nextState powinien miec wlasny counter
-
-
-//to powinno byc dopiero po calym jednym oddechu!
-void nextState(){//moze ustawiac kody greya?
+void nextMask(){//moze ustawiac kody greya?
   cli();//poczatek sekcji krytycznej
-  counter = 10;//                                   TODO
-  PORT &= ~state;
-  state>>=1;//dopasowanie do pinow kontrolera
-  state = (state+1) & 0xf;
-  state += (state > 0);//nie chce miec sytuacji gdzie wszystkie diody zgaszone
-  state<<=1;//dopasowanie do pinow kontrolera
+  counter = COUNTER;
+  PORT &= ~ledMask;
+  ledMask>>=1;//dopasowanie do pinow kontrolera
+  ledMask = (ledMask+1) & 0xf;
+  ledMask += (ledMask > 0);//nie chce miec sytuacji gdzie wszystkie diody zgaszone
+  ledMask<<=1;//dopasowanie do pinow kontrolera
   sei();//koniec sekcji krytycznej
 }
 
+void nextState(){
+  if(state==0){//dol licznika
+    ud = 1;//chce liczyc w gore
+    nextMask();
+  }
+  if(state==7){//gora licznika
+    ud = 0;//chce liczyc w dol
+    nextMask();
+  }
+  if(ud == 1)//up
+    state++;
+  else//down
+    state--;
+  // OCR0A = 1<<state;
+  OCR0A = (2<<state)-1;
+}
+
 ISR (TIM0_COMPA_vect){
-  PORT |= state;
+  PORT |= ledMask;
 }
 ISR (TIM0_OVF_vect){
-  PORT &= ~state;
+  PORT &= ~ledMask;
   counter--;
 }
 
 int main(){
   timer_init(); // uruchom licznik
+  sei();
   
   //Piny B 4321
   DDR |= 0b11110;
