dc426a46ed0e6b470c27005ac419ea16dd15c528
MaciejZientara <mtzientara@gmail.com>
2022-04-28 17:51:23 +0200

    dzialajace breathe z dodawaniem binarnym dla diod

--- wbudowane/random/drzewkoSzczescia/breathe.c
+++ wbudowane/random/drzewkoSzczescia/breathe.c
@@ -2,20 +2,13 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
-/*
-overflow timera nastepuje w regularnych odstepach czasu
-moge zrobic counter zmniejszany przy kazdym overflow
-w while usypiam jesli counter >0
-else zmieniam ustawienie portu (ktore diody zapalam)
-*/
-
 //counter to ile cykli zegara przy okreslonym wypelnieniu
 //ledMask to maska do PORT uzywana zapalania okreslonych diod
 //state odpowiada za wypelnienie (OCR0A = 1<<state)
 //w zaleznosci od ud (1=up,0=down) wykonuje state++/state--
 volatile uint8_t counter, state, ledMask, ud;
 // przypisuje counter=COUNTER
-#define COUNTER 10
+#define COUNTER 60
 
 #define PORT PORTB
 #define DDR DDRB
@@ -24,13 +17,12 @@ void timer_init(){
   // ustaw tryb licznika
   // fast PWM mode
   // WGM0  = 011 -- fast PWM top=0xFF
-  // CS0   = 100  -- prescaler 256
+  // CS0   = 010  -- prescaler 8
 
-  // częstotliwość 4e6/(256*(1+255)) = 61 Hz
-
+  // częstotliwość 4e6/(8*(1+255)) ~=~ 1953 Hz
   OCR0A = 1;
   TCCR0A = _BV(WGM01) | _BV(WGM00);
-  TCCR0B = /*_BV(WGM02) |*/ _BV(CS02);
+  TCCR0B = _BV(CS01);
 
   // wlacz interrupt na overflow i compare match
   TIMSK0 = _BV(OCIE0A) | _BV(TOIE0);
@@ -40,10 +32,8 @@ void nextMask(){//moze ustawiac kody greya?
   cli();//poczatek sekcji krytycznej
   counter = COUNTER;
   PORT &= ~ledMask;
-  ledMask>>=1;//dopasowanie do pinow kontrolera
-  ledMask = (ledMask+1) & 0xf;
-  ledMask += (ledMask > 0);//nie chce miec sytuacji gdzie wszystkie diody zgaszone
-  ledMask<<=1;//dopasowanie do pinow kontrolera
+  ledMask = (ledMask+1) & 0b1111;
+  ledMask += (ledMask == 0);//nie chce miec sytuacji gdzie wszystkie diody zgaszone
   sei();//koniec sekcji krytycznej
 }
 
@@ -54,21 +44,21 @@ void nextState(){
   }
   if(state==7){//gora licznika
     ud = 0;//chce liczyc w dol
-    nextMask();
   }
   if(ud == 1)//up
     state++;
   else//down
     state--;
-  // OCR0A = 1<<state;
-  OCR0A = (2<<state)-1;
+  counter = COUNTER;
+  // OCR0A = (state ? 1<<state : 1);
+  OCR0A = (2<<state)-2;
 }
 
 ISR (TIM0_COMPA_vect){
-  PORT |= ledMask;
+  PORT &= ~ledMask;
 }
 ISR (TIM0_OVF_vect){
-  PORT &= ~ledMask;
+  PORT |= ledMask;
   counter--;
 }
 
@@ -77,7 +67,7 @@ int main(){
   sei();
   
   //Piny B 4321
-  DDR |= 0b11110;
+  DDR |= 0b00001111;
 
   set_sleep_mode(SLEEP_MODE_IDLE);
   while(1) {
